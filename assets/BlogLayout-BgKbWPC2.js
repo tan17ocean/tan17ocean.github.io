9d369235cdd7ef32c3302b5c97360837a23f7d54
unknown <1515618169@qq.com>
2026-09-11 10:27:59 +0800

    deploy: login + admin dashboard

---
+++ assets/BlogLayout-BgKbWPC2.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./SideBar-xxq7ZMtF.js";import{d as t,e as a,g as e,D as c,h as _}from"./index-D2aPIM4S.js";const r={class:"page page-blog"},l={class:"blog-layout"},n={class:"blog-content"},g={__name:"BlogLayout",setup(d){return(o,i)=>(t(),a("div",r,[e("div",l,[e("div",n,[c(o.$slots,"default")]),_(s)])]))}};export{g as _};
