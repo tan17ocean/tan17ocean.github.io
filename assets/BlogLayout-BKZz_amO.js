248af4cce38e3900c3b004cd467e08a4e69ab8ae
unknown <1515618169@qq.com>
2026-09-15 08:07:52 +0800

    deploy: fix Jekyll ignoring underscore files (.nojekyll)

--- assets/BlogLayout-BKZz_amO.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./SideBar-C8Ev21N9.js";import{d as t,e as a,g as e,V as c,h as _}from"./index-5p_KT7VJ.js";const r={class:"page page-blog"},l={class:"blog-layout"},n={class:"blog-content"},g={__name:"BlogLayout",setup(d){return(o,i)=>(t(),a("div",r,[e("div",l,[e("div",n,[c(o.$slots,"default")]),_(s)])]))}};export{g as _};
